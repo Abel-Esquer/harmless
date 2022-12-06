61674b127f464e9d0e96babbd50a8c19b66947c1
Abel-Esquer <abel.esquer228663@potros.itson.edu.mx>
2022-12-06 01:40:18 -0700

    Primer commit

---
+++ includes/sidebar.php
@@ -0,0 +1,19 @@
+            <div class="sidebar">
+                <div class="sidebar-text d-flex flex-column h-100 justify-content-center text-center">
+                    <img class="mx-auto d-block w-75 bg-primary img-fluid rounded-circle mb-4 p-3" src="img/tareas.png" alt="Image">
+                    <h1 class="font-weight-bold">Curso</h1>
+                    <p class="mb-4">
+                        Listado de tareas
+                    </p>
+                    <div class="d-flex justify-content-center mb-5">
+                        <a class="btn btn-outline-primary mr-2" href="#">Tarea #1<i class=""></i></a>
+<!--                    <a class="btn btn-outline-primary mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-primary mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-primary mr-2" href="#"><i class="fab fa-instagram"></i></a> -->
+                    </div>
+                    <a href="" class="btn btn-primary mt-auto">Atras</a>
+                </div>
+                <div class="sidebar-icon d-flex flex-column h-100 justify-content-center text-right">
+                    <i class="fas fa-2x fa-angle-double-right text-primary"></i>
+                </div>
+            </div>
